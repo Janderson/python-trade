625bd9a94dc2ff38e50ba252b8c27a101eb98380
Janderson FFerreira <ffjanderson@gmail.com>
2017-04-20 21:51:26 -0300

    example using backtrader and ibapi
    Using lib backtrader and ibapi

---
+++ bt.ib.teste.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+import backtrader as bt
+
+class SmaCross(bt.SignalStrategy):
+    params = (('pfast', 10), ('pslow', 30),)
+    def __init__(self):
+        sma1, sma2 = bt.ind.SMA(period=self.p.pfast), bt.ind.SMA(period=self.p.pslow)
+        self.signal_add(bt.SIGNAL_LONG, bt.ind.CrossOver(sma1, sma2))
+
+cerebro = bt.Cerebro()
+
+ibstore = bt.stores.IBStore(host='127.0.0.1', port=7496, clientId=1001)
+data = ibstore.getdata(dataname='AAPL', timeframe=bt.TimeFrame.Seconds, compression=5)
+
+print(data)
+
+
+# data from yahoo finance
+#data = bt.feeds.YahooFinanceData(dataname='YHOO', fromdate=datetime(2011, 1, 1), todate=datetime(2012, 12, 31))
+cerebro.adddata(data)
+
+
+cerebro.addstrategy(SmaCross)
+print(cerebro.run())
+#cerebro.plot()
